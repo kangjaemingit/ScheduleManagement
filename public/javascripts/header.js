e19123fc59f32d6640a244f1d9f6a3b575ca9b97
R.Z.A <53036090+GunHeeKim127@users.noreply.github.com>
2022-10-05 08:57:41 +0900

    1005 상태창 색상 변경 완료

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,24 +1,27 @@
-function colorChange(){
-    document.getElementById("container").style.backgroundColor = document.getElementById("color").value;
-};
+
 // 팝업 열기
 function show() {
-    document.querySelector(".modal-bg").classList.add("show");
-    // document.getElementById("area1").style.backgroundColor = document.getElementById("color").value
-    // const element = document.getElementsByClassName('modal-bg');
-    //
-    // // 2. style 변경
-    // element.style.display = 'block';
+    const element1 = document.getElementById('modal-wrap');
+    // 2. style 변경
+    element1.style.display = 'block';
+    const element2 = document.getElementById('modal-bg');
+    // 2. style 변경
+    element2.style.display = 'block';
 }
 
 // 팝업 닫기
-function close() {
-    document.querySelector(".modal-bg").className = "background";
-    // const element = document.getElementsByClassName('modal-bg');
-    //
-    // // 2. style 변경
-    // element.style.display = 'hidden';
+function closed() {
+    const element1 = document.getElementById('modal-wrap');
+    // 2. style 변경
+    element1.style.display = 'none';
+    const element2 = document.getElementById('modal-bg');
+    // 2. style 변경
+    element2.style.display = 'none';
 }
+function colorChange(){
+    document.getElementById("headerContainer").style.backgroundColor = document.getElementById("color").value;
+};
 
-// document.querySelector("#show").addEventListener("click", show);
-// document.querySelector("#close").addEventListener("click", close);
+// document.getElementById('show').addEventListener('click',show);
+// document.querySelector('.modal-close').addEventListener('click',close);
+// document.getElementById('changeColor').addEventListener('click',colorChange);
