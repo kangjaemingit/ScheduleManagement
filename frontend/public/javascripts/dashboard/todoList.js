4f12a12d059c9d1588cad5fad37eab5c95c94ff2
강재민 <71342315+kangjaemingit@users.noreply.github.com>
2022-10-31 17:02:39 +0900

    피드, 투두리스트 파일 구조 수정

--- frontend/public/javascripts/dashboard/todoList.js
+++ frontend/public/javascripts/dashboard/todoList.js
@@ -1,4 +1,4 @@
-function addList()  {
+function createTodoList()  {
     const addBox=document.getElementById('todayScheduleBox');
     // 1. 추가할 값을 input창에서 읽어온다
     const addValue
@@ -23,29 +23,5 @@ function addList()  {
     addBox.appendChild(input)
     addBox.appendChild(div1)
     addBox.appendChild(div2)
-    addBox.style.display='flex'
-}
-function feedAddSelect()  {
-    const addBox=document.getElementById('feedContentContainer');
-    // 1. 추가할 값을 input창에서 읽어온다
-    const addValue
-        = document.getElementById('feedSelectContent').value;
-
-    // 2. 추가할 li element 생성
-    // 2-1. 추가할 li element 생성
-    const div1 = document.createElement("div");
-    const div2 = document.createElement('div')
-
-    // 2-2. li에 id 속성 추가
-    div1.setAttribute('id',addValue);
-    div2.setAttribute('id',addValue+'Date');
-
-    // 2-3. li에 text node 추가
-    const textNode = document.createTextNode(addValue);
-    div1.appendChild(textNode);
-
-    // 3. 생성된 li를 ul에 추가
-    addBox.appendChild(div1);
-    addBox.appendChild(div2);
     addBox.style.display='flex'
 }
