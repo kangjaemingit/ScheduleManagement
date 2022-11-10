90f944c2d9fadda908f73941a2d4cd1a38f549db
강재민 <71342315+kangjaemingit@users.noreply.github.com>
2022-11-10 17:41:31 +0900

    주석

--- backend/models/TodoList.js
+++ backend/models/TodoList.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const TodoListSchema = mongoose.Schema({
     todoListVal : {type : String},
     todoListWriter : {type : mongoose.Schema.Types.ObjectId, ref:"User"},
-    success:{type:String}
+    success:{type:Boolean}
 });
 
 const TodoList = mongoose.model('TodoList', TodoListSchema);
